40e4cdca6f7f21fa5c9ab1b0aee14e1f1b49bcb8
Nisha271 <bukkenisha@gmail.com>
2020-07-11 02:31:28 +0530

    Newely created Branch UC-1 and modified file and displayed Heads or tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,1 +1,11 @@
-echo "New Created Repo With Welcome Message"
+#!/bin/bash -x
+Heads=0
+Tails=0
+RandomCheck=$(( RANDOM%2 ))
+if [ $RandomCheck -eq 1 ]
+then
+        echo "Heads"
+else
+        echo "Tails"
+fi
+
