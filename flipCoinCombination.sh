92d2e1d93dc59932c6bb885bfd434205ce72ea89
Nisha271 <bukkenisha@gmail.com>
2020-07-11 03:01:16 +0530

    Newely created Branch UC-4 and modified file and calculate triplet combinatof Heads and Tales in Occurences and percentages

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-declare -A Doublet_Dictionary
+declare -A Triplet_Dictionary
 read -p "Enter the count for Coin Filp: " number
-Doublet_Dictionary[HH]=0
-Doublet_Dictionary[HT]=0
-Doublet_Dictionary[TH]=0
-Doublet_Dictionary[TT]=0
+Triplet_Dictionary[HHH]=0
+Triplet_Dictionary[HHT]=0
+Triplet_Dictionary[HTH]=0
+Triplet_Dictionary[THH]=0
+Triplet_Dictionary[HTT]=0
+Triplet_Dictionary[THT]=0
+Triplet_Dictionary[TTH]=0
+Triplet_Dictionary[TTT]=0
 
 counter=0
 flips=0
@@ -12,26 +16,30 @@ while [ $flips -lt $number ]
 do
         RandomCheck=$(( RANDOM%4 ))
         case $RandomCheck in
-                0 ) Doublet_Dictionary[HH]=$(( ${Doublet_Dictionary[HH]}+1 )) ;;
-                1 ) Doublet_Dictionary[HT]=$(( ${Doublet_Dictionary[HT]}+1 )) ;;
-                2 ) Doublet_Dictionary[TH]=$(( ${Doublet_Dictionary[TH]}+1 )) ;;
-                3 ) Doublet_Dictionary[TT]=$(( ${Doublet_Dictionary[TT]}+1 )) ;;
+                0 ) Triplet_Dictionary[HHH]=$(( ${Triplet_Dictionary[HHH]}+1 )) ;;
+                1 ) Triplet_Dictionary[HHT]=$(( ${Triplet_Dictionary[HHT]}+1 )) ;;
+                2 ) Triplet_Dictionary[HTH]=$(( ${Triplet_Dictionary[HTH]}+1 )) ;;
+                3 ) Triplet_Dictionary[THH]=$(( ${Triplet_Dictionary[THH]}+1 )) ;;
+                4 ) Triplet_Dictionary[HTT]=$(( ${Triplet_Dictionary[HTT]}+1 )) ;;
+                5 ) Triplet_Dictionary[THT]=$(( ${Triplet_Dictionary[THT]}+1 )) ;;
+                6 ) Triplet_Dictionary[TTH]=$(( ${Triplet_Dictionary[TTH]}+1 )) ;;
+                7 ) Triplet_Dictionary[TTT]=$(( ${Triplet_Dictionary[TTT]}+1 )) ;;
                 * ) echo "Invalid RandomCheck"
         esac
         flips=`expr $flips + 1`
 done
 
 echo "Occurences of Head and Tails"
-for i in "${!Doublet_Dictionary[@]}"
+for i in "${!Triplet_Dictionary[@]}"
 do
-      echo  $i : ${Doublet_Dictionary[$i]}
+      echo  $i : ${Triplet_Dictionary[$i]}
 done
 
 echo "Percentage Occurences of Heads and Tails"
-for i in "${!Doublet_Dictionary[@]}"
+for i in "${!Triplet_Dictionary[@]}"
 do
         result=$(( $number * 100 ))
-        echo $i ":" $( echo ${Doublet_Dictionary[$i]} $result | awk '{print $1 / $2 }' )
+        echo $i ":" $( echo ${Triplet_Dictionary[$i]} $result | awk '{print $1 / $2 }' )
 done
 
 
