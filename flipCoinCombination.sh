68d80d256fdc60a80a107aadc9042c7f146b3195
Nisha271 <bukkenisha@gmail.com>
2020-07-11 02:41:03 +0530

    Newely created Branch UC-2 and modified file and calculate Singlet combinatof Heads and Tales in Occurences and percentages

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,32 @@
 #!/bin/bash -x
-Heads=0
-Tails=0
-RandomCheck=$(( RANDOM%2 ))
-if [ $RandomCheck -eq 1 ]
-then
-        echo "Heads"
-else
-        echo "Tails"
-fi
+declare -A Singlet_Dictionary
+read -p "Enter the count for Coin Filp: " number
+Singlet_Dictionary[H]=0
+Singlet_Dictionary[T]=0
+counter=0
+flips=0
+while [ $flips -lt $number ]
+do
+        RandomCheck=$(( RANDOM%2 ))
+        case $RandomCheck in
+                0 ) Singlet_Dictionary[H]=$(( ${Singlet_Dictionary[H]}+1 )) ;;
+                1 ) Singlet_Dictionary[T]=$(( ${Singlet_Dictionary[T]}+1 )) ;;
+                * ) echo "Invalid RandomCheck"
+        esac
+        flips=`expr $flips + 1`
+done
+
+echo "Occurences of Head and Tails"
+for i in "${!Singlet_Dictionary[@]}"
+do
+      echo  $i : ${Singlet_Dictionary[$i]}
+done
+
+echo "Percentage Occurences of Heads and Tails"
+for i in "${!Singlet_Dictionary[@]}"
+do
+        result=$(( $number * 100 ))
+        echo $i ":" $( echo ${Singlet_Dictionary[$i]} $result | awk '{print $1 / $2 }' )
+done
+
 
