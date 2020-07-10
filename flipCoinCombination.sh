b61f4629021722254978b5e644f7f53d40ea86d2
Nisha271 <bukkenisha@gmail.com>
2020-07-11 02:51:28 +0530

    Newely created Branch UC-3 and modified file and calculate doublet combinatof Heads and Tales in Occurences and percentages

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,32 +1,37 @@
-#!/bin/bash -x
-declare -A Singlet_Dictionary
+#!/bin/bash
+declare -A Doublet_Dictionary
 read -p "Enter the count for Coin Filp: " number
-Singlet_Dictionary[H]=0
-Singlet_Dictionary[T]=0
+Doublet_Dictionary[HH]=0
+Doublet_Dictionary[HT]=0
+Doublet_Dictionary[TH]=0
+Doublet_Dictionary[TT]=0
+
 counter=0
 flips=0
 while [ $flips -lt $number ]
 do
-        RandomCheck=$(( RANDOM%2 ))
+        RandomCheck=$(( RANDOM%4 ))
         case $RandomCheck in
-                0 ) Singlet_Dictionary[H]=$(( ${Singlet_Dictionary[H]}+1 )) ;;
-                1 ) Singlet_Dictionary[T]=$(( ${Singlet_Dictionary[T]}+1 )) ;;
+                0 ) Doublet_Dictionary[HH]=$(( ${Doublet_Dictionary[HH]}+1 )) ;;
+                1 ) Doublet_Dictionary[HT]=$(( ${Doublet_Dictionary[HT]}+1 )) ;;
+                2 ) Doublet_Dictionary[TH]=$(( ${Doublet_Dictionary[TH]}+1 )) ;;
+                3 ) Doublet_Dictionary[TT]=$(( ${Doublet_Dictionary[TT]}+1 )) ;;
                 * ) echo "Invalid RandomCheck"
         esac
         flips=`expr $flips + 1`
 done
 
 echo "Occurences of Head and Tails"
-for i in "${!Singlet_Dictionary[@]}"
+for i in "${!Doublet_Dictionary[@]}"
 do
-      echo  $i : ${Singlet_Dictionary[$i]}
+      echo  $i : ${Doublet_Dictionary[$i]}
 done
 
 echo "Percentage Occurences of Heads and Tails"
-for i in "${!Singlet_Dictionary[@]}"
+for i in "${!Doublet_Dictionary[@]}"
 do
         result=$(( $number * 100 ))
-        echo $i ":" $( echo ${Singlet_Dictionary[$i]} $result | awk '{print $1 / $2 }' )
+        echo $i ":" $( echo ${Doublet_Dictionary[$i]} $result | awk '{print $1 / $2 }' )
 done
 
 
